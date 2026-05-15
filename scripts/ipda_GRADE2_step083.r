ff41764033b479718659ddaf27c842f02e655315
Isabela Almeida <mb.isabela42@gmail.com>
2026-05-15 14:46:54 +1000

    083 allow stem to be gene or transcript ID

--- scripts/ipda_GRADE2_step083.r
+++ scripts/ipda_GRADE2_step083.r
@@ -68,8 +68,41 @@ out_heatmap <- file.path(outdir, paste0(outstem, ".", plot_condition, ".heatmap.
 
 ## Import data/metadata
 counts <- fread(file.path(input_counts), data.table=FALSE)  # Use fread for speed if data.table available
+is_transcript <- outstem %in% counts$transcript
+is_gene <- outstem %in% counts$gene
+
+if (!is_transcript && !is_gene) {
+  stop(
+    paste0(
+      "'", outstem,
+      "' was not found in either transcript or gene columns."
+    )
+  )
+}
+
+if (is_transcript) {
+  counts <- counts %>%
+    filter(transcript == outstem)
+} else if (is_gene) {
+  counts <- counts %>%
+    filter(gene == outstem)
+  sample_cols <- setdiff(
+    colnames(counts),
+    c("transcript", "gene")
+  )
+  summed_counts <- colSums(
+    counts[, sample_cols, drop = FALSE],
+    na.rm = TRUE
+  )
+  counts <- data.frame(
+    transcript = outstem,
+    gene = outstem,
+    t(summed_counts),
+    check.names = FALSE
+  )
+}
 counts <- counts %>% select(-2) # gene column
-counts <- counts %>% filter(transcript == outstem)
+#counts2 <- counts %>% filter(transcript == outstem) # with the conditioning statement above user can provide either transcript or gene ID
 metadata <- read.delim(file.path(input_metadata), sep="\t", header=TRUE, row.names=1)
 metadata[[plot_condition]] <- factor(metadata[[plot_condition]]) # Ensure condition is factor
 
@@ -99,12 +132,12 @@ source(functions)
 
 ## Boxplot
 boxplot <- box_plot(tnorm_counts, plot_condition, outstem)
-ggsave(file.path(out_boxplot), plot = boxplot, width = 20, height = 20, dpi = 100)
+ggsave(file.path(out_boxplot), plot = boxplot, width = 20, height = 5, dpi = 100)
 
 ## Beeswarm
 beeswarm <- beeswarm_plot(tnorm_counts, plot_condition, outstem)
-ggsave(file.path(out_beeswarm), plot = beeswarm, width = 20, height = 20, dpi = 100)
+ggsave(file.path(out_beeswarm), plot = beeswarm, width = 20, height = 5, dpi = 100)
 
 ## Heatmap
 heatmap <- heatmap_plot(plot_heat)
-ggsave(file.path(out_heatmap), plot = heatmap, width = 20, height = 20, dpi = 100)
+ggsave(file.path(out_heatmap), plot = heatmap, width = 20, height = 5, dpi = 100)
--- scripts/ipda_grade2_rfunctions.r
+++ scripts/ipda_grade2_rfunctions.r
@@ -84,35 +84,97 @@ beeswarm_plot <- function(df, plot_condition, outstem) {
 # ------------------------------------------------------------
 # HEATMAP PLOT (GTEx-style)
 # Description: Log-scale distribution per group
-# Input: df (plot_condition_col, transcript, expr)
+# Input: plot_heat (plot_condition_col, plot_sec_condition, transcript, expr)
 # Output: ggplot object
-# Usage: heatmap_plot(df)
+# Usage: heatmap_plot(plot_heat)
 # ------------------------------------------------------------
 
 heatmap_plot <- function(df){
+  breaks <- c(0, 0.5, 1, 3, max(df$expr, na.rm = TRUE))
+
   gtex_colors <- c(
-    "#ffffcc",  # pale yellow
-    "#c7e9b4",  # light green
-    "#7fcdbb",  # teal
-    "#2c7fb8",  # blue
-    "#081d58"   # dark navy
+    "#ffffcc",  # 0      pale yellow
+    "#a0d97a",  # 0.5    light green
+    "#78c679",  # 1      green
+    "#41b6c4",  # 3      teal
+    "#081d58"   # max    navy
   )
-  
-  plot <- ggplot(plot_heat, aes(x = .data[[plot_condition]], y = source, fill = expr)) +
+
+  values <- scales::rescale(breaks)
+
+  plot1 <- ggplot(plot_heat, aes(x = .data[[plot_condition]], y = .data[[plot_sec_condition]], fill = expr)) +
   geom_tile(color = "white", height = 1, width = 1, linewidth = 0.3) +
   scale_y_discrete(position = "right") +
   scale_fill_gradientn(
     colors = gtex_colors,
+    values = values,
+    limits = c(0, max(df$expr, na.rm = TRUE)),
+    oob = scales::squish,
+    breaks = breaks,
+    labels = scales::label_number(accuracy = 0.01),
     na.value = "grey92",
     guide = guide_colorbar(
-        direction = "horizontal",
-        barwidth = 10,
-        barheight = 0.8,
-        ticks = TRUE,
-        frame.colour = "white"
+      direction = "vertical",
+      barwidth = 0.8,
+      barheight = 10,
+      ticks = TRUE,
+      frame.colour = "white"
   )) +
   labs(
-    fill = "log2(TPM + 1)"
+  fill = "log2(TPM + 1)\n(NA = grey)"
+  ) +
+  coord_fixed(ratio = 1) +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", colour = NA),
+    plot.background  = element_rect(fill = "white", colour = NA),
+    panel.grid       = element_blank(),
+    plot.margin = margin(t = 5.5, r = 5.5, b = 30, l = 5.5),
+    axis.title = element_blank(),
+    legend.position = "right",
+    legend.box = "vertical",
+    legend.justification = "center",
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 8),
+    legend.key.height = unit(1.5, "cm"),
+    legend.key.width  = unit(0.4, "cm"),
+    axis.text.y.right = element_text(
+      size = 8,
+      hjust = 0
+      ),
+    axis.text.x = element_text(
+      angle = 90,
+      hjust = 1,
+      vjust = 0.5,
+      size = 8,
+    ))
+  
+  plot2_df <- df %>%
+  dplyr::group_by(.data[[plot_condition]]) %>%
+  dplyr::summarise(
+    expr = sum(expr, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+  plot2 <- ggplot(plot2_df, aes(x = .data[[plot_condition]], y = outstem, fill = expr)) +
+  geom_tile(color = "white", height = 1, width = 1, linewidth = 0.3) +
+  scale_y_discrete(position = "right") +
+  scale_fill_gradientn(
+    colors = gtex_colors,
+    values = values,
+    limits = c(0, max(df$expr, na.rm = TRUE)),
+    oob = scales::squish,
+    breaks = breaks,
+    labels = scales::label_number(accuracy = 0.01),
+    guide = guide_colorbar(
+      direction = "horizontal",
+      barwidth = 10,
+      barheight = 0.8,
+      ticks = TRUE,
+      frame.colour = "white"
+  )) +
+  labs(
+  fill = "log2(TPM + 1)"
   ) +
   coord_fixed(ratio = 1) +
   theme_minimal() +
@@ -124,9 +186,11 @@ heatmap_plot <- function(df){
     axis.title = element_blank(),
     legend.position = "top",
     legend.box = "horizontal",
-    legend.justification = "left",
+    legend.justification = "center",
     legend.title = element_text(size = 8),
     legend.text = element_text(size = 8),
+    legend.key.height = unit(1.5, "cm"),
+    legend.key.width  = unit(0.4, "cm"),
     axis.text.y.right = element_text(
       size = 8,
       hjust = 0
@@ -137,6 +201,8 @@ heatmap_plot <- function(df){
       vjust = 0.5,
       size = 8,
     ))
+
+    plot <- gridExtra::grid.arrange(plot1, plot2, ncol = 1)
 
   return(plot)
 }
