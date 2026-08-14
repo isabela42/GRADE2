7cf51abfce0a1c390422613e06e8b93da33bf5a5
Isabela Almeida <mb.isabela42@gmail.com>
2026-08-14 15:12:19 +1000

    091 initial commit

---
+++ scripts/ipda_GRADE2_step091-to-pbs.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+
+version="1.0.0"
+usage(){
+echo "
+Written by Isabela Almeida with input from Larissa Cassiano
+Created on Aug 11, 2026
+Last modified on Aug 14, 2026
+Version: ${version}
+
+Description: Write and submit PBS jobs for step 091 of the
+GRADE2 PBS 2.0 pipeline (General RNAseq Analysis for Differential Expression version 2).
+
+Usage: bash ipda_GRADE2_step091-to-pbs.sh -i "path/to/input/files" -p "PBS stem" -e "email" -m INT -c INT -w "HH:MM:SS"
+
+Resources used for pipeline in-house: -m 5 -c 4 -w "05:00:00"
+
+## Input:
+
+-i <path/to/input/files>    Input TSV files including path FROM working
+                            directory. This TSV file should contain:
+                            
+                            Col1:
+                            sample-stem
+
+                            Col2:
+                            /path/from/working/dir/to/grade051_alignment_STAR_*/salign-star-index_stem/salign-star-index_stemAligned.sortedByCoord.out.bam
+                            Or similar BAM file
+
+                            Col3:
+                            /path/from/working/dir/to/reference.genome.fasta
+                            Note: must have chromosome ids matching the annotation used for generating Col2 file.
+
+                            It does not matter if same stem 
+                            appears more than once on this input file.
+
+-p <PBS stem>               Stem for PBS file names
+-e <email>                  Email for PBS job
+-m <INT>                    Memory INT required for PBS job in GB
+-c <INT>                    Number of CPUS required for PBS job
+-w <HH:MM:SS>               Clock walltime required for PBS job
+
+## Output:
+
+logfile.txt                 Logfile with commands executed and date
+PBS files                   PBS files created
+
+Pipeline description:
+
+#   000 Index building (0gffcompare, 1Kallisto, 2RSEM, 3STAR, 4Salmon)
+#   010 Quality check raw files (0Bedtools, 1FastQC, 2MultiQC)
+#   020 Trim reads of adapters (1Trimmomatic)
+#   030 Quality check trimmed files (1FastQC, 2MultiQC)
+#   040 Pseudo align and quantify reads (1Kallisto, 2BASH count tables)
+#   050 Align (1STAR, 2SAMtools, 3NovoSort) and quantify reads (4RSEM, 5BASH count tables)
+#   060 PSeudo align and quantify reads at isoform level (1Salmon, 2BASH count tables)
+#   070 Differential Expression Analysis (1EdgeR)
+#   080 Plot counts and draw metrics (1pca+heatmap+boxplot, 2metrics, 3id plots)
+#-->091 Allele-specific expression stats (1BCFtools, 2ASEP)
+
+Please contact Isabela Almeida at mb.isabela42@gmail.com if you encounter any problems.
+"
+}
+
+## Display help message
+if [ -z "$1" ] || [[ $1 == -h ]] || [[ $1 == --help ]]; then
+	usage
+	exit
+fi
+
+#  ____       _   _   _                 
+# / ___|  ___| |_| |_(_)_ __   __ _ ___ 
+# \___ \ / _ \ __| __| | '_ \ / _` / __|
+#  ___) |  __/ |_| |_| | | | | (_| \__ \
+# |____/ \___|\__|\__|_|_| |_|\__, |___/
+#                             |___/     
+
+## Save execution ID
+pid=`echo $$` #$BASHPID
+
+## User name within your cluster environment
+user=`whoami`
+
+## Exit when any command fails
+set -e
+
+#................................................
+#  Input parameters from command line
+#................................................
+
+## Get parameters from command line flags
+while getopts "i:p:e:m:c:w:h:v" flag
+do
+    case "${flag}" in
+        i) input="${OPTARG}";;       # Input files including path
+        p) pbs_stem="${OPTARG}";;    # Stem for PBS file names
+        e) email="${OPTARG}";;       # Email for PBS job
+        m) mem="${OPTARG}";;         # Memory required for PBS job
+        c) ncpus="${OPTARG}";;       # Number of CPUS required for PBS job
+        w) walltime="${OPTARG}";;    # Clock walltime required for PBS job
+        h) Help ; exit;;             # Print Help and exit
+        v) echo "${version}"; exit;; # Print version and exit
+        ?) echo script usage: bash ipda_grade2_step091-to-pbs.sh -i path/to/input/files -p PBS stem -e email -m INT -c INT -w "HH:MM:SS" >&2
+           exit;;
+    esac
+done
+
+#................................................
+#  Set Logfile stem
+#................................................
+
+## Set Logfile stem
+# it contains all the executed commands with date/time;
+# the output files general metrics (such as size);
+# and memory/CPU usage for all executions
+thislogdate=$(date +'%d%m%Y%H%M%S%Z')
+human_thislogdate=`date`
+logfile=logfile_ipda_GRADE2_step091-to-pbs_${thislogdate}.txt
+
+#................................................
+#  Set and create output path
+#................................................
+
+## Set stem for output directories
+outpath_GRADE2091_ase="grade091_allele-specific-expression_BCFtools_${thislogdate}"
+
+## Create output directories
+mkdir -p ${outpath_GRADE2091_ase}
+
+#................................................
+#  Required modules, softwares and libraries
+#................................................
+
+# BCFtools/1.22
+module_bcftools="bcftools/1.22"
+
+#................................................
+#  Print Execution info to user
+#................................................
+
+date
+echo "## Executing bash ipda_grade2_step091-to-pbs.sh"
+echo "## This execution PID: ${pid}"
+echo
+echo "## Given inputs:"
+echo
+echo "## Input files:                 ${input}"
+echo "## PBS stem:                    ${pbs_stem}"
+echo "## Email for PBS notifications: ${email}"
+echo "## PBS job memory required:     ${mem}"
+echo "## PBS job NCPUS required:      ${ncpus}"
+echo "## PBS job walltime required:   ${walltime}"
+echo
+echo "## Outputs created:"
+echo
+echo "## Output files saved to:       ${outpath_GRADE2091_ase}"
+echo "## logfile will be saved as:    ${logfile}"
+echo
+
+# ____  _             _   _                   
+#/ ___|| |_ __ _ _ __| |_(_)_ __   __ _       
+#\___ \| __/ _` | '__| __| | '_ \ / _` |      
+# ___) | || (_| | |  | |_| | | | | (_| |_ _ _ 
+#|____/ \__\__,_|_|   \__|_|_| |_|\__, (_|_|_)
+#                                 |___/  
+
+#................................................
+#  Print Execution info to logfile
+#................................................
+
+exec &> "${logfile}"
+
+date
+echo "## Executing bash ipda_grade2_step091-to-pbs.sh"
+echo "## This execution PID: ${pid}"
+echo
+echo "## Given inputs:"
+echo
+echo "## Input files:                 ${input}"
+echo "## PBS stem:                    ${pbs_stem}"
+echo "## Email for PBS notifications: ${email}"
+echo "## PBS job memory required:     ${mem}"
+echo "## PBS job NCPUS required:      ${ncpus}"
+echo "## PBS job walltime required:   ${walltime}"
+echo
+echo "## Outputs created:"
+echo
+echo "## Output files saved to:       ${outpath_GRADE2091_ase}"
+echo "## This is logfile:             ${logfile}"
+
+set -v
+
+#................................................
+#  Create PBS files
+#................................................
+
+## Write PBS header
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#!/bin/sh" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "##########################################################################" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Script:  ${pbs_stem}_${stem}_${thislogdate}.pbs" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Author:  Isabela Almeida" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Created: ${human_thislogdate} at QIMR Berghofer (Brisbane, Australia) - VSC" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Updated: ${human_thislogdate} at QIMR Berghofer (Brisbane, Australia) - VSC" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Version: v01" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Email:   ${email}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "##########################################################################" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+## Write PBS directives
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#PBS -N ${pbs_stem}_${stem}_${thislogdate}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#PBS -r n" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#PBS -l mem=${mem}GB,walltime=${walltime},ncpus=${ncpus}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#PBS -m abe" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#PBS -M ${email}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+## Write directory setting
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Set main working directory" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "## Change to main directory" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo 'cd ${PBS_O_WORKDIR}' >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo 'echo ; echo "WARNING: The main directory for this run was set to ${PBS_O_WORKDIR}"; echo ' >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+## Write load modules
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Load Softwares, Libraries and Modules" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "module load ${module_bcftools}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+
+## Write PBS command lines
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#  Run step" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "#................................................" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo 'echo "## Index BAM file at" ; date ; echo' >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do file=`grep "${stem}" ${input} | cut -f2 | sort | uniq`; echo "samtools index -@${ncpus} ${file}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+cut -f1 ${input} | sort | uniq | while read stem; do echo 'echo "## Write het ref and alt allele counts at" ; date ; echo' >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do file=`grep "${stem}" ${input} | cut -f2 | sort | uniq`; ref=`grep "${stem}" ${input} | cut -f3 | sort | uniq`; echo "bcftools mpileup --threads ${ncpus} -d0 -Ou -Q 20 -q 20 -f ${ref} ${file} | bcftools call --threads ${ncpus} -m -Ob -a GQ,GP | bcftools query -i 'GT=\"het\"' -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT\t%AD\t%GQ\t%GP]\n' | awk -F'\t' 'BEGIN{OFS=\"\t\"; print \"CHROM\", \"SAMPLE\", \"ID\", \"REF_COUNT\", \"TOTAL_COUNT\", \"GROUP\", \"GQ\", \"P_HOM_REF\", \"P_HET\", \"P_HOM_ALT\"}} {split(\$4, alt, \",\"); split(\$6, ad, \",\"); gq = \$7; split(\$8, gp, \",\"); if (length(alt) == 1 && alt[1] != \"<*>\") {print \$1, \"${stem}\", \$1\":\"\$2, ad[1], ad[1] + ad[2], \$1, gq, gp[1], gp[2], gp[3]}}' >> ${outpath_hippo111_samtools}/${stem}.het.full.counts ; cut -f1-6 ${outpath_hippo111_samtools}/${stem}.het.full.counts > ${outpath_hippo111_samtools}/${stem}.het.4asep.counts" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+
+#................................................
+#  Submit PBS jobs
+#................................................
+
+## Submit PBS jobs 
+ls ${pbs_stem}_*${thislogdate}.pbs | while read pbs; do echo ; echo "#................................................" ; echo "# This is PBS: ${pbs}" ;  echo "#" ; echo "# main command line(s): $(tail -n1 ${pbs})" ; echo "#" ; echo "# now submitting PBS" ; echo "qsub ${pbs}" ; qsub ${pbs} ; echo "#................................................" ; done
+
+date ## Status of all user jobs (including GRADE2 step 091 jobs) at
+qstat -u "$user"
+
+# This will remove $VARNAMES from output file with the actual $VARVALUE
+# allowing for easily retracing commands
+sed -i 's,${input},'"${input}"',g' "$logfile"
+sed -i 's,${pbs_stem},'"${pbs_stem}"',g' "$logfile"
+sed -i 's,${email},'"${email}"',g' "$logfile"
+sed -i 's,${mem},'"${mem}"',g' "$logfile"
+sed -i 's,${ncpus},'"${ncpus}"',g' "$logfile"
+sed -i 's,${walltime},'"${walltime}"',g' "$logfile"
+sed -i 's,${human_thislogdate},'"${human_thislogdate}"',g' "$logfile"
+sed -i 's,${thislogdate},'"${thislogdate}"',g' "$logfile"
+sed -i 's,${user},'"${user}"',g' "$logfile"
+sed -i 's,${module_bcftools},'"${module_bcftools}"',g' "$logfile"
+sed -i 's,${outpath_GRADE2091_ase},'"${outpath_GRADE2091_ase}"',g' "$logfile"
+sed -i 's,${logfile},'"${logfile}"',g' "$logfile"
+sed -n -e :a -e '1,3!{P;N;D;};N;ba' $logfile > tmp ; mv tmp $logfile
+set +v
